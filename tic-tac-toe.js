e3420fb40a8ee0a542d5e83c7bbdd166b6304b1a
gary <baby_plant@icloud.com>
2021-04-02 15:13:16 -0700

    Add row win conditions

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
 
   const board = document.getElementById('tic-tac-toe-board');
+  const gameStatus = document.getElementById('game-status');
   let player = 'X';
   let boardState = ['', '', '', '', '', '', '', '', ''];
 
@@ -9,25 +10,53 @@ window.addEventListener('DOMContentLoaded', () => {
     const square = document.getElementById(event.target.id); // ask a TA "why *wouldn't* we just suse this every time?"
     if (!square.style.backgroundImage) {
       const squareID = square.id.slice(7);
-      // console.log(squareID)
+      //const squareID = square.id[square.id.length - 1]
+
+      console.log(squareID)
       if (player === 'X') {
         square.classList.add('X')
         square.style.backgroundImage = "url(images/player-x.svg)";
         // boardState.splice(squareID, 1, "x")
-        boardState[squareID] = "x"
+        boardState[squareID] = "X"
         player = 'O';
       } else if (player === 'O') {
         square.classList.add('O')
         square.style.backgroundImage = "url(images/player-o.svg)";
-        boardState[squareID] = "x"
+        boardState[squareID] = "O"
         player = 'X';
       }
       console.log(boardState)
-
+      winner(boardState)
     }
 
 
 
   })
+
+  const winner = (boardArray) => {
+
+    //rows
+    if (boardArray[0] === boardArray[1] && boardArray[0] === boardArray[2] && boardArray[0] !== '') {
+
+      gameStatus.innerText = `Winner : ${boardArray[0]}`
+
+    }
+    if (boardArray[3] === boardArray[4] && boardArray[3] === boardArray[5] && boardArray[3] !== '') {
+
+      gameStatus.innerText = `Winner : ${boardArray[3]}`
+
+    }
+    if (boardArray[6] === boardArray[7] && boardArray[6] === boardArray[8] && boardArray[6] !== '') {
+
+      gameStatus.innerText = `Winner : ${boardArray[6]}`
+
+    }
+
+
+    //column
+
+    //diag
+
+  }
 
 })
