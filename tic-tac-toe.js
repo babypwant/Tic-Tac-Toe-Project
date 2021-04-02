0c3c0b333d0b59bcd1fa1dce23a7f3299b5a1ea7
wilson <whuang3015@gmail.com>
2021-04-02 14:35:07 -0700

    Work on refactoring in a board state

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,20 +2,27 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const board = document.getElementById('tic-tac-toe-board');
   let player = 'X';
+  let boardState = ['', '', '', '', '', '', '', '', ''];
 
   board.addEventListener('click', event => {
 
-    const square = document.getElementById(event.target.id);
+    const square = document.getElementById(event.target.id); // ask a TA "why *wouldn't* we just suse this every time?"
     if (!square.style.backgroundImage) {
+      const squareID = square.id.slice(7);
+      // console.log(squareID)
       if (player === 'X') {
-
+        square.classList.add('X')
         square.style.backgroundImage = "url(images/player-x.svg)";
+        // boardState.splice(squareID, 1, "x")
+        boardState[squareID] = "x"
         player = 'O';
       } else if (player === 'O') {
-
+        square.classList.add('O')
         square.style.backgroundImage = "url(images/player-o.svg)";
+        boardState[squareID] = "x"
         player = 'X';
       }
+      console.log(boardState)
 
     }
 
